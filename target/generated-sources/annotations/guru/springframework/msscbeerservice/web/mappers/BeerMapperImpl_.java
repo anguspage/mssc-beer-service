8085514843c40269e498128fee392867c5938f83
Jesús Antonio Sarco Giannini <jesus.antonio.sarco@gmail.com>
2020-08-15 00:18:17 +0200

    commit

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-14T21:33:43+0200",
+    date = "2020-08-15T00:06:37+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
