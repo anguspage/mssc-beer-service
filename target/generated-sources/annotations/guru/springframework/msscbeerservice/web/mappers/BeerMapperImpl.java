1bef2add8961fe43898e1380db592a013e754b80
Jesús Antonio Sarco Giannini <jesus.antonio.sarco@gmail.com>
2020-08-14 18:11:48 +0200

    Updating Parent version - closes #45

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-13T01:35:34+0200",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
+    date = "2020-08-14T17:57:40+0200",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
 @Primary
