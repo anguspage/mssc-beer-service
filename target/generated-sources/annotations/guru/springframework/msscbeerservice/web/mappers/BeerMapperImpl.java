a837e23c50559c129a60e1f53b0bbd1556e921fe
Jesús Antonio Sarco Giannini <jesus.antonio.sarco@gmail.com>
2020-08-16 23:11:21 +0200

    Integration Test for New to Picked Up Written - closes #30

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-08-16T18:08:05+0200",
+    date = "2020-08-16T23:01:33+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
